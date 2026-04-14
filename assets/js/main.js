9fed7fef75807dbddfcbd0979bee62f0c1a42460
Cris Neil John D. Hulleza <crisneilhulleza32@gmail.com>
2026-04-14 15:27:39 +0800

    Fix navigation links

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,16 +9,6 @@ $(document).ready(function() {
         }, 500);
     });
 
-    // Smooth scroll for navigation links
-    $('.nav-links a').on('click', function(e) {
-        e.preventDefault();
-        const target = $(this).attr('href');
-        
-        $('html, body').animate({
-            scrollTop: $(target).offset()?.top || 0
-        }, 800);
-    });
-
     // Add hover effect to nav links
     $('.nav-links a').hover(
         function() {
